60f95a38480654614b5fd10210b4a47636149d5a
Anocha <lnw_oom@hotmail.com>
2018-01-08 18:07:27 +0700

    add insert and update database

--- public/index.php
+++ public/index.php
@@ -2,42 +2,69 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Database information
+$settings = [
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'todo-api',
+    'username' => 'root',
+    'password' => '',
+    'collation' => 'utf8_general_ci',
+    'prefix' => ''
+];
+
+// Bootstrap Eloquent ORM
+$capsule = new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($settings);
+$capsule->setEventDispatcher(new Illuminate\Events\Dispatcher(new Illuminate\Container\Container));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
 $app = new Slim\App();
 
 $app->get('/v1/todos', function($req, $res, $args) {
-  return $res->withJson([
-  'total' => 0,
-  'per_page' => 0,
-  'current_page' => 0,
-  'last_page' => 0,
-  'first_page_url' => 'string',
-  'last_page_url' => 'string',
-  'next_page_url' => 'string',
-  'prev_page_url' => 'string',
-  'path' => 'string',
-  'from' => 0,
-  'to' => 0,
-  'data' => [
-    [
-      "id"=> 0,
-      "name"=> "string",
-      "status"=> true,
-      "created_at"=> "2018-01-08T07:48:11.609Z",
-      "updated_at"=> "2018-01-08T07:48:11.609Z"
-    ]
-  ]]);
+
+	$page = (int) $req->getParam('page');
+	$todos = App\Models\Todo::orderBy('created_at', 'desc')->paginate(15, ['*'], 'page', $page);
+
+	$todos->setPath((string) $req->getUri()->withQuery(''));
+
+	return $res->withJson($todos);
 });
 
 $app->post('/v1/todos', function($req, $res, $args) {
-  return $res->withStatus(201);
+	$params = $req->getParams();
+
+	if(!isset($params['name'])) return $res->withStatus(403);
+
+	$todo = new App\Models\Todo;
+	$todo->name = $params['name'];
+	$todo->save();
+
+	return $res->withStatus(201);
 });
 
 $app->patch('/v1/todos/{id}', function($req, $res, $args) {
-  return $res->withStatus(204);
+	$params = $req->getParams();
+	$id = $args['id'];
+
+	if(!isset($params['name']) && !isset($params['status'])) return $res->withStatus(403);
+
+	$todo = App\Models\Todo::find($id);
+	if(isset($params['name']))$todo->name = $params['name'];
+	if(isset($params['status']))$todo->status = $params['status'];
+	$todo->save();
+
+	return $res->withStatus(204);
 });
 
 $app->delete('/v1/todos/{id}', function($req, $res, $args) {
-  return $res->withStatus(204);
+
+	$todo = App\Models\Todo::find(1);
+
+	$todo->delete();
+
+	return $res->withStatus(204);
 });
 
 $app->run();
