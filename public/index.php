7aa44bea725b4e3ab4accc3f071e65d3ed8c7cde
Maetad Sukarasud <maetad91@gmail.com>
2018-01-08 15:05:29 +0700

    add routes

--- public/index.php
+++ public/index.php
@@ -1,2 +1,23 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/v1/todos', function($req, $res, $args) {
+  return $res->withJson(['message' => 'get todo']);
+});
+
+$app->post('/v1/todos', function($req, $res, $args) {
+  return $res->withJson(['message' => 'post todo']);
+});
+
+$app->patch('/v1/todos/{id}', function($req, $res, $args) {
+  return $res->withJson(['message' => 'patch todo ' . $args['id']]);
+});
+
+$app->delete('/v1/todos/{id}', function($req, $res, $args) {
+  return $res->withJson(['message' => 'delete todo ' . $args['id']]);
+});
+
+$app->run();
